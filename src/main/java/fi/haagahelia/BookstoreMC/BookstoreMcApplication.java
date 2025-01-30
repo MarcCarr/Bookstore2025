3cb718853458cab9e587eae04b2fae89baeedca2
MarcCarr <bhj362@myy.haaga-helia.fi>
2025-01-30 08:49:14 +0200

    First commit

---
+++ src/main/java/fi/haagahelia/BookstoreMC/BookstoreMcApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.BookstoreMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/BookstoreMC/BookstoreMcApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.BookstoreMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
