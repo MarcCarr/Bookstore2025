f100347398f64489da88f7465bc939fd71dcd18a
MarcCarr <bhj362@myy.haaga-helia.fi>
2025-02-05 21:21:26 +0200

    Revamp templates + delete action

--- src/main/java/fi/haagahelia/BookstoreMC/controller/BookController.java
+++ src/main/java/fi/haagahelia/BookstoreMC/controller/BookController.java
@@ -3,7 +3,11 @@ package fi.haagahelia.BookstoreMC.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import fi.haagahelia.BookstoreMC.domain.Book;
 import fi.haagahelia.BookstoreMC.domain.BookstoreRepository;
 
 @Controller
@@ -20,4 +24,24 @@ public class BookController {
         return "booklist";
     }
 
+    // Open add new book form
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    // Save new book into list
+    @PostMapping("/save")
+    public String saveNewBook(@ModelAttribute Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    // Deleting a book
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        repository.deleteById(id);
+        return "redirect:/booklist";
+    }
 }
--- src/main/java/fi/haagahelia/BookstoreMC/domain/BookstoreRepository.java
+++ src/main/java/fi/haagahelia/BookstoreMC/domain/BookstoreRepository.java
@@ -3,9 +3,6 @@ package fi.haagahelia.BookstoreMC.domain;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookstoreRepository extends CrudRepository<Book, Long>{
-    List<Book> findByTitle(String title);
-    List<Book>findByAuthor(String author);
+public interface BookstoreRepository extends CrudRepository<Book, Long> {
+    List<Book> findAll();
 }
-
-
