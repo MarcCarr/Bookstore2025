ab6045ae7e0f5ff36fcf6d8f90f39c9c632527a0
MarcCarr <bhj362@myy.haaga-helia.fi>
2025-02-13 10:10:56 +0200

    More REST

--- src/main/java/fi/haagahelia/BookstoreMC/controller/BookRestController.java
+++ src/main/java/fi/haagahelia/BookstoreMC/controller/BookRestController.java
@@ -3,13 +3,12 @@ package fi.haagahelia.BookstoreMC.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import fi.haagahelia.BookstoreMC.domain.Book;
 import fi.haagahelia.BookstoreMC.domain.BookstoreRepository;
 
-@Controller
+@RestController
 public class BookRestController {
 
     private BookstoreRepository bookRepository;
@@ -21,19 +20,19 @@ public class BookRestController {
 
     // RESTful service to get all students
     @GetMapping("/books")
-    public @ResponseBody List<Book> bookListRest() {
+    public List<Book> bookListRest() {
         return (List<Book>) bookRepository.findAll();
     }
 
     // RESTful service to get student by id
     @GetMapping("/books/{id}")
-    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+    public Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
         return bookRepository.findById(bookId);
     }
 
     // RESTful service to save new student
     @PostMapping("/books")
-    public @ResponseBody Book saveNewBookRest(@RequestBody Book book) {
+    public Book saveNewBookRest(@RequestBody Book book) {
         return bookRepository.save(book);
     }
 }
--- src/main/java/fi/haagahelia/BookstoreMC/domain/BookstoreRepository.java
+++ src/main/java/fi/haagahelia/BookstoreMC/domain/BookstoreRepository.java
@@ -1,8 +1,15 @@
 package fi.haagahelia.BookstoreMC.domain;
 
 import java.util.List;
-import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+@RepositoryRestController
 public interface BookstoreRepository extends CrudRepository<Book, Long> {
     List<Book> findAll();
+
+    List<Book> findByTitle(@Param("title") String title);
+
 }
